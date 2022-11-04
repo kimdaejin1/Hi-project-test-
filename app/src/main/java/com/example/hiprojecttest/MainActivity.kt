ef7a00308bc41993a487fc88c88ffe22d47eeacc
dragin01 <s22040@gsm.hs.kr>
2022-11-04 14:03:36 +0900

    makeing test commit

--- app/src/main/java/com/example/hiprojecttest/MainActivity.kt
+++ app/src/main/java/com/example/hiprojecttest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.hiprojecttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hiprojecttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
+
 }
