98c454b1240368d95955980c4340a05326bd54b3
dragin01 <s22040@gsm.hs.kr>
2022-11-07 20:30:22 +0900

    makeing test commit

--- app/src/main/java/com/example/hiprojecttest/LoginActivity.kt
+++ app/src/main/java/com/example/hiprojecttest/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.example.hiprojecttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hiprojecttest.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
